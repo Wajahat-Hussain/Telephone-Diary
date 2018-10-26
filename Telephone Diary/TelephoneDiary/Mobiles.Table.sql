3388ecd66c1ee9beea93dd6ec54f9cc23952a916
hwajahat <myaddress90@gmail.comgit config --global user.name hwajahatgit config --global user.myaddress90@gmail.com>
2018-10-26 22:29:59 +0500

    telephone diary

---
+++ Telephone Diary/TelephoneDiary/Mobiles.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[First] NVARCHAR(50) NULL, 
+    [Last] NVARCHAR(50) NULL, 
+    [Mobile] NVARCHAR(50) NOT NULL, 
+    [Email] VARCHAR(50) NULL, 
+    [Category] VARCHAR(50) NULL, 
+    CONSTRAINT [PK_Table] PRIMARY KEY ([Mobile]) 
+)
---
+++ Telephone Diary/TelephoneDiary/TelephoneDiary/Phone.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TelephoneDiary
+{
+    public partial class Phone : Form
+    {
+        SqlConnection conn = new SqlConnection("Data Source=WAJAHATHUSSAIN\\SQLEXPRESS;Initial Catalog=Phone;Integrated Security=True");
+
+        public Phone()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Clear();
+            textBox3.Text = "";
+            textBox4.Clear();
+            comboBox1.SelectedIndex = -1;
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(@"INSERT INTO Mobiles (First,Last,Mobile,Email,Category)
+				                            VALUES('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','" + comboBox1.Text + "')",conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Successfully Saved..");
+            Display();
+        }
+
+        void Display()
+        {
+            SqlDataAdapter sda = new SqlDataAdapter("Select * from Mobiles",conn);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.Rows.Clear();
+            foreach (DataRow item in dt.Rows)
+            {
+                int n = dataGridView1.Rows.Add();
+                dataGridView1.Rows[n].Cells[0].Value = item["First"].ToString();
+                dataGridView1.Rows[n].Cells[1].Value = item["Last"].ToString();
+                dataGridView1.Rows[n].Cells[2].Value = item["Mobile"].ToString();
+                dataGridView1.Rows[n].Cells[3].Value = item["Email"].ToString();
+                dataGridView1.Rows[n].Cells[4].Value = item["Category"].ToString();
+            }
+        }
+
+        private void Phone_Load(object sender, EventArgs e)
+        {
+            Display();
+        }
+
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            comboBox1.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(@"UPDATE Mobiles
+                                       SET First ='" + textBox1.Text + "', Last ='" + textBox2.Text + "', Mobile ='" + textBox3.Text + "', Email ='" + textBox4.Text + "', Category ='" + comboBox1.Text + "'      WHERE (Mobile = '" + textBox3.Text + "')", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Update Successfully..");
+            Display();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(@"DELETE FROM Mobiles
+                                       WHERE (Mobile = '" + textBox3.Text + "')",conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Successfully Deleted..");
+            Display();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            SqlDataAdapter sda = new SqlDataAdapter("Select * from Mobiles      WHERE (Mobile like '%" + textBox5.Text + "%') or (First like '%" + textBox5.Text + "%') or (Last like '%" + textBox5.Text + "%')", conn);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.Rows.Clear();
+            foreach (DataRow item in dt.Rows)
+            {
+                int n = dataGridView1.Rows.Add();
+                dataGridView1.Rows[n].Cells[0].Value = item["First"].ToString();
+                dataGridView1.Rows[n].Cells[1].Value = item["Last"].ToString();
+                dataGridView1.Rows[n].Cells[2].Value = item["Mobile"].ToString();
+                dataGridView1.Rows[n].Cells[3].Value = item["Email"].ToString();
+                dataGridView1.Rows[n].Cells[4].Value = item["Category"].ToString();
+            }
+        }
+    }
+}
+
